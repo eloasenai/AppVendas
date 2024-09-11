75a978e117e15a9ba63e9c4ae096d0d2e2b7e052
ELOÁ SILVA <francodosreissilvae@gmail.com>
2024-09-11 14:12:08 -0300

    Adicionar arquivos de projeto.

---
+++ AppvVendas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppvVendas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
